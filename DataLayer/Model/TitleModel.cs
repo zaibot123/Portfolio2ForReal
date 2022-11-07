6db88d6d70c411253b9e60f73a8a627a9d2e1c10
Tobia <tobias@taekker.net>
2022-11-07 13:12:06 +0100

    g

--- DataLayer/Model/TitleModel.cs
+++ DataLayer/Model/TitleModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,7 @@ namespace DataLayer.Model
     {
         public string TitleName { get; set; }
         public string Poster { get; set; }
+
+
     }
 }
